de3b0dde5a7a211514e4fc2cd29ab6bd1c145dee
kiranp7 <147871503+kiranp7@users.noreply.github.com>
2024-07-15 13:36:31 +0530

    Add files via upload

---
+++ library_management/library_management/doctype/article/article.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Kiran P and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Article", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ library_management/library_management/doctype/library_setting/library_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Kiran P and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Library Setting", {
+// 	refresh(frm) {
+
+// 	},
+// });
